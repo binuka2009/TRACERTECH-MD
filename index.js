4fe09f5a559585788a29e9e266d44f8b7a9fa4cc
binuka2009 <binukaudayanga3@gmail.com>
2025-10-28 22:29:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-const fs = require("fs").promises;
-const path = require("path");
-const express = require("express");
-const config = require("./config");
-const { connect, writeSession, patch, parseDir, sleep } = require("./lib");
-const { getandRequirePlugins } = require("./lib/db/plugins");
+import fs from "fs/promises";
+import path from "path";
+import express from "express";
+import config from "./config.js";
+import { connect, writeSession, patch, parseDir, sleep } from "./lib/index.js";
+import { getandRequirePlugins } from "./lib/db/plugins.js";
 
 class BotSystem {
    constructor() {
-      global.__basedir = __dirname;
+      global.__basedir = path.resolve();
       this.app = express();
       this.port = process.env.PORT || 3000;
    }
 
    async initialize() {
       try {
-         await Promise.all([patch(), parseDir(path.join(__dirname, "/lib/db/")), parseDir(path.join(__dirname, "/plugins/")), this.ensureTempDir(), this.createGitignore()]);
+         await Promise.all([
+            patch(),
+            parseDir(path.join(global.__basedir, "/lib/db/")),
+            parseDir(path.join(global.__basedir, "/plugins/")),
+            this.ensureTempDir(),
+            this.createGitignore(),
+         ]);
 
          await sleep(2000);
-         console.log("Syncing Database");
+         console.log("Syncing Database...");
          await config.DATABASE.sync();
          await writeSession();
          await getandRequirePlugins();
-         console.log("External Modules Installed");
+         console.log("External Modules Installed ✅");
          return await connect();
       } catch (error) {
          console.error("Initialization error:", error);
@@ -29,11 +35,12 @@ class BotSystem {
    }
 
    startServer() {
-      this.app.get("/", (req, res) => res.send("Bot Running"));
+      this.app.get("/", (req, res) => res.send("Bot Running ✅"));
+      this.app.listen(this.port, () => console.log(`Server running on port ${this.port}`));
    }
 
    async ensureTempDir() {
-      const dir = path.join(__dirname, "temp");
+      const dir = path.join(global.__basedir, "temp");
       await fs.mkdir(dir, { recursive: true });
    }
 
@@ -53,7 +60,7 @@ temp`;
          await this.initialize();
          this.startServer();
       } catch (error) {
-         console.warn("BOT SYSTEM FAILED", error);
+         console.warn("BOT SYSTEM FAILED ⚠️", error);
       }
    }
 }
